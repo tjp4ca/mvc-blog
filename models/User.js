619d52a6d066d86289766167fb95cc58fcf85988
Michael Park <tjp4ca@gmail.com>
2022-08-24 02:46:57 -0400

    models hashed pw

---
+++ models/User.js
@@ -0,0 +1,90 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        hooks: {
+          async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          },
+
+          
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
+
+
+// *** notes
+
+// salt
+// const hashPassword = async () => {
+//     const salt = await bcrypt.genSalt(10);
+//     console.log(salt);
+// }
+// hashPassword();
+
+// salt + hash
+// const hashPassword = async (pw) => {
+//     const salt = await bcrypt.genSalt(10);
+//     const hash = await bcrypt.hash(pw, salt)
+//     console.log(salt);
+//     console.log(hash);
+// }
+
+// const login = async (pw, hashedPw) => {
+//     const result = await bcrypt.compare(pw, hashedPw);
+//     if (result) {
+//         console.log('Logged In Successfully!')
+//     } else {
+//         console.log('Incorrect!')
+//     }
+// }
+
+// hashPassword('monkey');
+
+
+// const hashPassword = async (pw) => {
+//     const hash = await bcrypt.hash(pw, 10)
+//     console.log(hash);
+// }
+// hashPassword('monkey')
+
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// const controllers = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(controllers);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
+
+// sequelize.sync({ force: false }).then(() => {
+//     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+// });
