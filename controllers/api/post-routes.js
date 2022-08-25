c9abb7b17f0ab1cc5de61c525d1c1f05a1c9b7a2
Michael Park <tjp4ca@gmail.com>
2022-08-24 22:13:07 -0400

    routes updated

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,3 +4,49 @@ const { Post, User, Comment } = require('../../models');
 
 // const withAuth = require('../../utils/auth');
 
+router.get('/', (req, res) => {
+    Post.findAll({
+        include: [
+            {
+                model: Comment
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Comment
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require('express');
-// const controllers = require('./controllers');
+const controllers = require('./controllers');
+
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,7 +32,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(controllers);
+app.use(controllers);
 
 // app.listen(PORT, () => {
 //   console.log(`App listening on port ${PORT}!`);
